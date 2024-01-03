87c4274599d22656d9f2bd434ccb4fe412dff3ad
Simon Gutenbrunner <s2210595005@students.fh-hagenberg.at>
2024-01-03 17:05:38 +0100

    Added Javadoc

--- src/main/java/at/clc/calculator/controllers/CalculatorRestController.java
+++ src/main/java/at/clc/calculator/controllers/CalculatorRestController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * The CalculatorRestController Class holds the implementation for all REST endpoints
+ */
 @RestController
 public class CalculatorRestController {
 
@@ -16,6 +19,12 @@ public class CalculatorRestController {
         this.calculatorService = calculatorService;
     }
 
+    /**
+     * Adds the two request params together
+     * @param a first request param
+     * @param b second request param
+     * @return sum of request params a + b
+     */
     @GetMapping("/add")
     public String add(@RequestParam int a, @RequestParam int b) {
         return String.valueOf(calculatorService.add(a,b));
--- src/main/java/at/clc/calculator/services/CalculatorService.java
+++ src/main/java/at/clc/calculator/services/CalculatorService.java
@@ -2,9 +2,18 @@ package at.clc.calculator.services;
 
 import org.springframework.stereotype.Service;
 
+/**
+ * The CalculatorService holds the implementation for all calculation logic used by the application.
+ */
 @Service
 public class CalculatorService {
 
+    /**
+     * Adds two numbers a and b
+     * @param a first summand
+     * @param b second sumand
+     * @return sum of a + b
+     */
     public int add(int a, int b){
         return a+b;
     }
