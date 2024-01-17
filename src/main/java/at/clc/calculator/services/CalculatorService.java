30ebe797b9cbe4465395c74e9aa26cc8c975556a
Simon Gutenbrunner <s2210595005@students.fh-hagenberg.at>
2024-01-17 15:08:59 +0100

    updated docs

--- src/main/java/at/clc/calculator/services/CalculatorService.java
+++ src/main/java/at/clc/calculator/services/CalculatorService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 /**
  * The CalculatorService holds the implementation for all calculation logic used by the application.
- * test docs ADADADDADADADAD
+ * test docs 2
  */
 @Service
 public class CalculatorService {
